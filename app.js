756391cf03cea786deccce00756d30cf97de1fac
Furkan Uzhan <132701916+Hardresetf7@users.noreply.github.com>
2023-06-19 16:24:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let chosenDoor = prompt("Chose a door to get in.")
+
+if(chosenDoor ==1){
+    alert("Chosen door is "+ chosenDoor);
+}
+else if(chosenDoor==2){
+    alert("Chosen door is "+ chosenDoor);
+}
+else if(chosenDoor==3){
+    alert("Chosen door is "+ chosenDoor);
+}
+else{
+    alert("Press chose a avaible door number.");
+}
